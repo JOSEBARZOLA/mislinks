cf04661afacf7dba3f800587876f2b8956d59bd6
JOSEBARZOLA <jose_barzola89@hotmail.com>
2023-05-11 01:00:42 -0300

    erroes en rutas

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
-const {isLoggedIn} = require('../lib/auth');
 
-
-
-router.get('/add', isLoggedIn, (req, res) =>{
-res.render('./links/add');
+router.get('/add', (req, res) =>{
+res.render('links/add');
 });
 
-router.post('/add', isLoggedIn, async (req, res) =>{
+router.post('/add', async (req, res) =>{
     const {title, url, description } = req.body;
 const newLink ={
 title,
@@ -23,26 +20,26 @@ res.redirect('/links');
 
 
 
-router.get('/list',isLoggedIn, async (req, res) => {
+router.get('/', async (req, res) => {
 const links = await pool.query('SELECT * FROM links');
-res.render('/links/list', {links});
+res.render('links/list', {links});
 });
 
-router.get('/delete/:id', isLoggedIn, async (req, res) =>{
+router.get('/delete/:id', async (req, res) =>{
    const { id } = req.params;
    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
    req.flash('success', 'Enlace borrado');
 res.redirect('/links');
 });
 
-router.get('/edit/:id', isLoggedIn,  async (req, res) =>{
+router.get('/edit/:id', async (req, res) =>{
     const { id } = req.params;
     const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
     res.render('./links/edit', {link: links[0]});
  });
 
 
- router.post('/edit/:id',  async (req,res) =>{
+ router.post('/edit/:id', async (req,res) =>{
     const { id } = req.params;
     const { title, url, description } = req.body;
 const newLink ={
