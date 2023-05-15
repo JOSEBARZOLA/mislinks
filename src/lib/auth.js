89d0fa5fa3b55bd53837610f032324f26c2fbd66
JOSEBARZOLA <jose_barzola89@hotmail.com>
2023-05-15 17:34:35 -0300

    proteccion de rutas

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,5 +5,14 @@ if(req.isAuthenticated()){
 return next();
 }
 return res.redirect('/signin');
+},
+
+isNotLoggedIn(req, res, next){
+    if(!req.isAuthenticated()) {
+        return next();
+        }
+        return res.redirect('/profile');
 }
+
+
 };
